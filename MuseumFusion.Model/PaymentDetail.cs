479c8308a4645c8b40bc21572295c3858f1b4e12
ssrtssrtssrt <73035958+ssrtssrtssrt@users.noreply.github.com>
2023-12-09 14:33:36 +0530

    Add files via upload

---
+++ MuseumFusion.Model/PaymentDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuseumFusion.Model;
+public class PaymentDetail
+{
+    public string MuseumName { get; set; }
+    public string CardNumber { get; set; }
+    public string ExpirationDate { get; set; }
+    public string CVV { get; set; }
+    public string CardHolderName { get; set; }
+    public DateTime PaymentDateTime { get; set; }
+    public double Price { get; set; }
+    public double Tax { get; set; }
+    public double TotalAmount { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# dotnet-project
+# dotnet-project
